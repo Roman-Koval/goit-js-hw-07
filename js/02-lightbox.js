8aed9ef850e494440923119afbdd05eaa6e80dcc
Roman-Koval <kovalrv83@gmail.com>
2021-11-15 00:01:54 +0200

    second solved

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+  
+  function createGalleryItems() {
+      const galleryMarkup = galleryItems.map(({ original, preview, description }) => {
+          return `<a class="gallery__item" href=${original}>
+          <img class="gallery__image" src=${preview} alt=${description} title=${description}"/>
+        </a>`
+      }
+      ).join('');
+      return galleryMarkup;
+      
+  }
+let gallery = new SimpleLightbox(".gallery a");
+
+gallery.on("show.simplelightbox", function () {
+  gallery.options.captionDelay = 250;
+});
